43630b58b8e3e23ca1a177b6491ab0f94544d450
Allie O'Brien <obrienaj@s.dcsdk12.org>
2019-08-21 13:51:25 -0600

    Appreciate Sophie

--- Git.java
+++ Git.java
@@ -2,8 +2,9 @@ public class Git {
     public static void main(String args[]){
         System.out.println("\"git is fun!!!!!!!!\"");
         System.out.println("Hi Mom");
-        System.out.println("Scout is the best cat")
-        System.out.println("Sammy has the best purr")
+        System.out.println("Scout is the best cat");
+        System.out.println("Sammy has the best purr");
+        System.out.println("Sophie is a princess");
     }
 
 }
